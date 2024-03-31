4f804a0e585c2fd9865a927e7f5d435bcc3e7ce6
JA_X <envijs93@gmail.com>
2024-03-31 22:10:17 +0900

    Feat: share 페이지 컴포넌트 추가

--- pages/shared.tsx
+++ pages/shared.tsx
@@ -1,17 +1,24 @@
-import Nav from "@/components/\bLayout/Nav";
-import Header from "@/components/\bLayout/Header";
+import Nav from "@/components/Layout/Nav";
+import Header from "@/components/Layout/Header";
 import SearchBar from "@/components/common/SearchBar";
 import CardList from "@/components/common/CardList";
-import Footer from "@/components/\bLayout/Footer";
+import Footer from "@/components/Layout/Footer";
+import useFetchData from "@/hooks/useFetchData";
+import styles from "../styles/Shared.module.css";
 
 export default function Shared() {
+  const data = useFetchData("sampleFolder");
+  const currentUserData = useFetchData("sampleUser");
+
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <>
-      <Nav />
-      <Header />
+    <div className={styles.SharedContainer}>
+      <Nav currentUserData={currentUserData} />
+      <Header folderName={data.name} owner={data.owner} />
       <SearchBar />
-      <CardList />
+      <CardList cardDataList={data.links} />
       <Footer />
-    </>
+    </div>
   );
 }
