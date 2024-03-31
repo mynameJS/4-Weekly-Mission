109825882af82fadbe32d6fa1573c4330852e1f4
JA_X <envijs93@gmail.com>
2024-03-31 22:05:40 +0900

    Feat: 외부이미지 렌더링 오류 해결하기 위해 domain 작성

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "codeit-front.s3.ap-northeast-2.amazonaws.com",
+      "codeit-images.codeit.com",
+      "www.shutterstock.com",
+      "jasonwatmore.com",
+      "codeit-frontend.codeit.com",
+      "reactjs.org",
+      "assets.vercel.com",
+      "tanstack.com",
+      "storybook.js.org",
+      "testing-library.com",
+      "static.cdninstagram.com",
+      "s.pstatic.net",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
