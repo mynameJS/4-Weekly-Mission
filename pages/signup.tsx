465f45001c333e27621c53194009527159070f9b
JA_X <envijs93@gmail.com>
2024-04-08 18:58:40 +0900

    Feat: 회원가입 페이지 유효성 유저인풋 유효성검사 로직 추가

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -6,8 +6,82 @@ import eyeOff from "@/public/Images/eye-off.svg";
 import google from "@/public/Images/google.png";
 import kakao from "@/public/Images/Kakao.svg";
 import styles from "@/styles/Sign.module.css";
+import { validatePasswordInput } from "@/utils/validate";
+import { checkEmailValidate } from "@/utils/api";
+import { useState } from "react";
+
+interface ShowState {
+  [key: string]: boolean;
+}
 
 export default function SignUp() {
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const [isShow, setIsShow] = useState<ShowState>({
+    password: false,
+    passwordConfirm: false,
+  });
+
+  const toggleIsShow = (key: string) => {
+    setIsShow((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+  };
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInput({
+      ...userInput,
+      [name]: value,
+    });
+  };
+
+  const onBlurEmailInput = async (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const result = await checkEmailValidate(value);
+    if (result.error) {
+      setErrorMessage({
+        ...errorMessage,
+        [name]: result.error.message,
+      });
+    } else {
+      setErrorMessage({
+        ...errorMessage,
+        [name]: "",
+      });
+    }
+  };
+
+  const onBlurPasswordInput = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "passwordConfirm") {
+      const isPasswordValid = value === userInput.password;
+      setErrorMessage({
+        ...errorMessage,
+        [name]: isPasswordValid ? "" : "비밀번호가 일치하지 않아요",
+      });
+      return;
+    }
+
+    setErrorMessage({
+      ...errorMessage,
+      [name]: validatePasswordInput(value)
+        ? ""
+        : "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+    });
+  };
+
   return (
     <div className={styles.body}>
       <div className={styles.header}>
@@ -40,7 +114,12 @@ export default function SignUp() {
                 id="email"
                 name="email"
                 placeholder="이메일을 입력해 주세요."
+                onChange={onChangeInput}
+                onBlur={onBlurEmailInput}
               />
+              {errorMessage.email && (
+                <p className={styles.errorMessage}>{errorMessage.email}</p>
+              )}
             </div>
             <div className={`${styles.signInputBox} ${styles.signPassword}`}>
               <label className={styles.signInputLabel} htmlFor="password">
@@ -48,13 +127,28 @@ export default function SignUp() {
               </label>
               <input
                 className={styles.signInput}
-                type="password"
+                type={isShow.password ? "text" : "password"}
                 id="password"
                 name="password"
                 placeholder="영문, 숫자를 조합해 8자 이상 입력해 주세요."
+                onChange={onChangeInput}
+                onBlur={onBlurPasswordInput}
               />
-              <button className={styles.eyeButton} type="button" tabIndex={-1}>
-                <Image src={eyeOff} alt="눈동자가림" width={20} height={20} />
+              {errorMessage.password && (
+                <p className={styles.errorMessage}>{errorMessage.password}</p>
+              )}
+              <button
+                className={styles.eyeButton}
+                type="button"
+                tabIndex={-1}
+                onClick={() => toggleIsShow("password")}
+              >
+                <Image
+                  src={isShow.password ? eyeOn : eyeOff}
+                  alt="눈동자가림"
+                  width={20}
+                  height={20}
+                />
               </button>
             </div>
             <div className={`${styles.signInputBox} ${styles.signPassword}`}>
@@ -66,13 +160,30 @@ export default function SignUp() {
               </label>
               <input
                 className={styles.signInput}
-                type="password"
+                type={isShow.passwordConfirm ? "text" : "password"}
                 id="passwordConfirm"
                 name="passwordConfirm"
                 placeholder="비밀번호와 일치하는 값을 입력해 주세요."
+                onChange={onChangeInput}
+                onBlur={onBlurPasswordInput}
               />
-              <button className={styles.eyeButton} type="button" tabIndex={-1}>
-                <Image src={eyeOff} alt="눈동자가림" width={20} height={20} />
+              {errorMessage.passwordConfirm && (
+                <p className={styles.errorMessage}>
+                  {errorMessage.passwordConfirm}
+                </p>
+              )}
+              <button
+                className={styles.eyeButton}
+                type="button"
+                tabIndex={-1}
+                onClick={() => toggleIsShow("passwordConfirm")}
+              >
+                <Image
+                  src={isShow.passwordConfirm ? eyeOn : eyeOff}
+                  alt="눈동자가림"
+                  width={20}
+                  height={20}
+                />
               </button>
             </div>
           </div>
